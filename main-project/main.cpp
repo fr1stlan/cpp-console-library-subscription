b5f848b1135ad33f083266056d17cc7ad202027d
fr1stlan <eyeless.jack.soup@gmail.com>
2024-05-30 14:14:56 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "");
     cout << "Лабораторная работа #8. GIT\n";
     cout << "Вариант #9. Телефонные разговоры\n";
     cout << "Автрор: Станислав Сотников\n";
